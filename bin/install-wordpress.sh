4823bc4d1c5cc54a1ee55bb2e7eb1a0c40a51284
Paul Gibbs <djpaul@gmail.com>
2019-01-28 14:19:03 +0000

    Revert "Extend wp-cli test"
    This reverts commit 28fa1a095e8537b25d0f2eb5f79d1cc8a85e7364.

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -2,6 +2,6 @@
 set -ex
 
 docker exec -it --user www-data docker_wpcli_1 config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
-docker exec -it --user www-data docker_wpcli_1 core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
-docker exec -it --user www-data docker_wpcli_1 theme activate --path="$TRAVIS_BUILD_DIR" twentyseventeen
-docker exec -it --user www-data docker_wpcli_  rewrite structure --path="$TRAVIS_BUILD_DIR" '/%year%/%monthnum%/%postname%/'
+#vendor/bin/wp core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
+#vendor/bin/wp theme activate --path="$TRAVIS_BUILD_DIR" twentyseventeen
+#vendor/bin/wp rewrite structure --path="$TRAVIS_BUILD_DIR" '/%year%/%monthnum%/%postname%/'
