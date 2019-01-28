ebfbc1d3ff907ea7770a6d5d73dfbcb267dd42ad
Paul Gibbs <djpaul@gmail.com>
2019-01-28 14:19:12 +0000

    Revert "Try adding wp-cli container"
    This reverts commit f249295b39271fc20fda5e8dbbd3f6488cb0602d.

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 
-docker exec -it --user www-data docker_wpcli_1 config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
-#vendor/bin/wp core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
-#vendor/bin/wp theme activate --path="$TRAVIS_BUILD_DIR" twentyseventeen
-#vendor/bin/wp rewrite structure --path="$TRAVIS_BUILD_DIR" '/%year%/%monthnum%/%postname%/'
+export TRAVIS_BUILD_DIR="$1"
+
+vendor/bin/wp config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
+vendor/bin/wp core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
+vendor/bin/wp theme activate --path="$TRAVIS_BUILD_DIR" twentyseventeen
+vendor/bin/wp rewrite structure --path="$TRAVIS_BUILD_DIR" '/%year%/%monthnum%/%postname%/'
