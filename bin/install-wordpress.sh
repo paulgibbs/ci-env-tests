d8e1faa629bd12463b8d69df9cbfe58782cd1d77
Paul Gibbs <djpaul@gmail.com>
2019-01-28 16:03:23 +0000

    Don't create new wp-config

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -3,7 +3,6 @@ set -ex
 
 WORDPRESS_DIR=/var/www/html
 
-vendor/bin/wp config create --path="$WORDPRESS_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
 vendor/bin/wp core install --path="$WORDPRESS_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
 vendor/bin/wp theme activate --path="$WORDPRESS_DIR" twentyseventeen
 vendor/bin/wp rewrite structure --path="$WORDPRESS_DIR" '/%year%/%monthnum%/%postname%/'
