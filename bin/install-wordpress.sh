57abc777f13875c7bcdfcce9a8f681bc0af36cce
Paul Gibbs <djpaul@gmail.com>
2019-01-28 11:48:06 +0000

    Remove WP env vars (not set) from script

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-vendor/bin/wp config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}"
+vendor/bin/wp config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
 vendor/bin/wp core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
 vendor/bin/wp theme activate --path="$TRAVIS_BUILD_DIR" twentyseventeen
 vendor/bin/wp rewrite structure --path="$TRAVIS_BUILD_DIR" '/%year%/%monthnum%/%postname%/'
