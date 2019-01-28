4ee2801ab31e0fbb1f9ac5cf5eb205b48fa2cd0b
Paul Gibbs <djpaul@gmail.com>
2019-01-28 15:56:33 +0000

    Fix WordPress directory

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-export TRAVIS_BUILD_DIR="$1"
+WORDPRESS_DIR=/var/www/html
 
-vendor/bin/wp config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
-vendor/bin/wp core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
-vendor/bin/wp theme activate --path="$TRAVIS_BUILD_DIR" twentyseventeen
-vendor/bin/wp rewrite structure --path="$TRAVIS_BUILD_DIR" '/%year%/%monthnum%/%postname%/'
+vendor/bin/wp config create --path="$WORDPRESS_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
+vendor/bin/wp core install --path="$WORDPRESS_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
+vendor/bin/wp theme activate --path="$WORDPRESS_DIR" twentyseventeen
+vendor/bin/wp rewrite structure --path="$WORDPRESS_DIR" '/%year%/%monthnum%/%postname%/'
