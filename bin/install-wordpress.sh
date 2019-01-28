b27cf845c3dc34ebacff8490431adb15af1721a9
Paul Gibbs <djpaul@gmail.com>
2019-01-28 13:13:57 +0000

    Refrain from `set -u` since it breaks Travis.
    See travis-ci/travis-ci#5434

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+set -ex
 
 vendor/bin/wp config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
 vendor/bin/wp core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
