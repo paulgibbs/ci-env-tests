f249295b39271fc20fda5e8dbbd3f6488cb0602d
Paul Gibbs <djpaul@gmail.com>
2019-01-28 14:06:40 +0000

    Try adding wp-cli container

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -ex
 
-export TRAVIS_BUILD_DIR="$1"
-
-vendor/bin/wp config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
-vendor/bin/wp core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
-vendor/bin/wp theme activate --path="$TRAVIS_BUILD_DIR" twentyseventeen
-vendor/bin/wp rewrite structure --path="$TRAVIS_BUILD_DIR" '/%year%/%monthnum%/%postname%/'
+docker exec -it --user www-data docker_wpcli_1 config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
+#vendor/bin/wp core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
+#vendor/bin/wp theme activate --path="$TRAVIS_BUILD_DIR" twentyseventeen
+#vendor/bin/wp rewrite structure --path="$TRAVIS_BUILD_DIR" '/%year%/%monthnum%/%postname%/'
