5aa1d72e37ee1b3b56b4a135099890795d500d65
Paul Gibbs <djpaul@gmail.com>
2019-01-28 13:19:15 +0000

    Debugging.

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+echo "$TRAVIS_BUILD_DIR"
+echo "${TRAVIS_BUILD_DIR}"
+
 vendor/bin/wp config create --path="$TRAVIS_BUILD_DIR" --dbhost='db' --dbname='wordpress' --dbuser="wordpress" --dbpass="wordpress"
 vendor/bin/wp core install --path="$TRAVIS_BUILD_DIR" --url='http://wordpress/' --title="RS WordPress Install" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
 vendor/bin/wp theme activate --path="$TRAVIS_BUILD_DIR" twentyseventeen
